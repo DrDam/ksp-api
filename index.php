4f7d8148c9bf3084213d01553c52229c5cf559ac
DrDam <DrDam8584@gmail.com>
2018-04-28 19:09:54 +0200

    header origin

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
 require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
 
 use Symfony\Component\HttpFoundation\Request;
